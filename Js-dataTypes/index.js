8ae5048a5dbfd8d93371526fadbd835cfd29dc5d
Manikandan <manikandan.m1313@gmail.com>
2026-05-23 05:17:16 +0530

    JS data types

---
+++ Js-dataTypes/index.js
@@ -0,0 +1,45 @@
+let name = "mani";
+
+console.log(typeof name);
+
+const age = 20.1;
+console.log(typeof age);
+
+const isStudent = true;
+console.log(typeof isStudent);
+
+const marks = null;
+console.log(typeof marks);
+
+const subject = undefined;
+console.log(typeof subject);
+
+const person = {
+  name: "mani",
+  age: 20,
+  isStudent: true,
+  marks: null,
+  subject: undefined,
+};
+console.log(typeof person);
+
+const arr = [1, 2, 3, 4, 5];
+console.log(typeof arr);
+
+const date = new Date();
+console.log(typeof date);
+
+const regex = /mani/;
+console.log(typeof regex);
+
+const symbol = Symbol("mani");
+console.log(typeof symbol);
+
+const func = function () {};
+console.log(typeof func);
+
+const obj = new Object();
+console.log(typeof obj);
+
+const set = new Set([1, 2, 3]);
+console.log(typeof set);
