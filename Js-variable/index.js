4672e4a801863b1b4444c5221fcd07260f5ae31f
Manikandan <manikandan.murugan@businesscoresolutions.com>
2026-05-23 04:34:05 +0530

    JS variable

---
+++ Js-variable/index.js
@@ -0,0 +1,30 @@
+// variable
+
+var a = 10;
+var b = 20;
+var a = 30;
+console.log(a + b);
+
+let c = 10;
+let d = 20;
+c = 30;
+console.log(c + d);
+
+const e = 10;
+const f = 20;
+console.log(e + f);
+
+//var
+// re declare
+// re assign
+// global scope
+
+//let
+// re declare
+// re assign
+// block scope
+
+//const
+// not re declare
+// not re assign
+//  block scope
