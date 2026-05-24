9fe5d0f1e24f08ff4fef36a76f7a8577bc14572c
Manikandan <manikandan.m1313@gmail.com>
2026-05-25 05:13:56 +0530

    JS functions

--- Js-functions/index.js
+++ Js-functions/index.js
@@ -10,12 +10,33 @@ function userLog() {
 
 userLog();
 
-async function getUsers() {
-  let response = await fetch("https://jsonplaceholder.typicode.com/users");
+// async function getUsers() {
+//   let response = await fetch("https://jsonplaceholder.typicode.com/users");
 
-  let data = await response.json();
+//   let data = await response.json();
 
-  console.log(data);
+//   console.log(data);
+// }
+
+// getUsers();
+
+function sum(a, b) {
+  return a + b;
 }
 
-getUsers();
+console.log(sum(10, 20));
+
+function secondLargest(arr) {
+  let largest = arr[0];
+  let secondLargest = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > largest) {
+      secondLargest = largest;
+      largest = arr[i];
+    } else if (arr[i] > secondLargest && arr[i] < largest) {
+      secondLargest = arr[i];
+    }
+  }
+  return secondLargest;
+}
+console.log(secondLargest([1, 2, 3, 4, 5]));
