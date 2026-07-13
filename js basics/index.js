d70faf525c883b4724eaeea19b8e60ffabe1bcee
Manikandan <manikandan.murugan@businesscoresolutions.com>
2026-07-13 07:56:34 +0530

    js basics

--- js basics/index.js
+++ js basics/index.js
@@ -1,1 +1,28 @@
 console.log('hello world')
+
+function sum(...numbers) {
+    console.log(numbers);
+}
+
+sum(2)
+
+
+let a =1;
+
+
+for(i=a;i<=100;i++){
+    console.log(i)
+}
+
+
+for(i=a;i<=100;i++){
+ if(i % 2 == 0){
+      console.log("Even No: " + i)
+ }
+}
+
+for(i=a;i<=100;i++){
+ if(i % 2 != 0){
+      console.log("Odd No: " + i)
+ }
+}
